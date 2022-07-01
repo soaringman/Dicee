3d97b69c982aef948d5a9f6abc05048d551b362d
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-07-01 15:21:16 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Алексей Гуляев on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
